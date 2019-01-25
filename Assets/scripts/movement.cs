9c440f5b63d17b57310c03a9ab922a391b6aad68
FraserKenny1996 <fraserkenny999@hotmail.com>
2019-01-25 21:15:49 +0000

    mov,quad,placeholder
    added a 'movement' script, a quad as a floor and a scalled 3d model as a placeholder

---
+++ Assets/scripts/movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    public Rigidbody rb;
+    private bool moving = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check for movment input
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        //update movement and rotation
+        rb.velocity = new Vector3(x * speed, 0, y * speed);
+        moving = true;
+        Debug.Log("moving");
+
+        if (moving = true) ;
+        {
+            speed = speed + Time.deltaTime;
+            if (speed > 5.0f)
+            {
+                Debug.Log(gameObject.transform.position.x + " : " + speed);
+            }
+        }
+        //play movement animation while moving
+
+    }
+
+}
+
