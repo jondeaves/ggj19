f4154c3e8f5c7d7d35be7eabc62043cf91af22de
FraserKenny <FKENNY200@caledonian.ac.uk>
2019-01-26 10:50:06 +0000

    Dog Asset and script started

---
+++ Assets/scripts/GoodBoyFolder/HeckingBite.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeckingBite : MonoBehaviour {
+
+	[SerializeField]
+	private readonly int PlayerNumber = 1;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+		{
+
+			if (Input.GetButtonUp("Action " + PlayerNumber))
+			{
+				Collider[] hitColliders = Physics.OverlapSphere(transform.position, 1.5f);
+
+				foreach (Collider col in hitColliders)
+				{
+					if (col.tag == "theif")
+					{
+						col.gameObject.SetActive(false);
+					}
+				}
+			}
+	}
+}
