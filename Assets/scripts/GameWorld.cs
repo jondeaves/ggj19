50fad626c3b8e7d95ecf11d2e75c7bb15e7e10ec
Jon Deaves <hello@jondeaves.me>
2019-01-26 11:29:46 +0000

    feat: escape room
    can use door to escape only within last 10 seconds of song

---
+++ Assets/scripts/GameWorld.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameWorld : MonoBehaviour
+{
+    private List<GameObject> m_EscapedThiefs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_EscapedThiefs = new List<GameObject>();
+    }
+
+    public void AddEscapee(GameObject obj)
+    {
+        if (obj.tag == "thief")
+        {
+            Debug.Log(obj.tag + " has escaped");
+            m_EscapedThiefs.Add(obj);
+        }
+    }
+}
--- Assets/scripts/Loot/LootBag.cs
+++ Assets/scripts/Loot/LootBag.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class LootBag : MonoBehaviour
@@ -7,39 +6,4 @@ public class LootBag : MonoBehaviour
     [SerializeField]
     [Tooltip("All items currently in the thiefs loot bag")]
     public List<GameObject> Loot;
-
-    private int m_PlayerNumber = 1;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        m_PlayerNumber = GetComponent<Movement>().PlayerNumber;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetButtonUp("Action " + m_PlayerNumber))
-        {
-            Collider[] hitColliders = Physics.OverlapSphere(transform.position, 1.5f);
-
-            foreach (Collider col in hitColliders)
-            {
-                if (col.tag == "loot" && Loot.FindAll((GameObject obj) => obj == col.gameObject).Count == 0)
-                {
-                    Debug.Log("Thief got some loot");
-                    Loot.Add(col.gameObject);
-                    col.gameObject.SetActive(false);
-                }
-            }
-        }
-    }
-
-    public void AddLoot(GameObject obj)
-    {
-        if (obj.tag == "loot")
-        {
-            Loot.Add(obj);
-        }
-    }
 }
