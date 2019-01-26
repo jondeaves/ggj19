cb04da6f78f32174f0e7f4e51559fef1a750f61d
Jon Deaves <hello@jondeaves.me>
2019-01-26 13:29:16 +0000

    feat: add shitty menus

---
+++ Assets/scripts/Instructions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Instructions : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0) || Input.GetButtonUp("Submit"))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
---
+++ Assets/scripts/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public Button m_SubmitBtn;
+    public Button m_QuitBtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_SubmitBtn.onClick.AddListener(OnSubmit);
+        m_QuitBtn.onClick.AddListener(OnQuit);
+    }
+
+    private void OnSubmit()
+    {
+        Debug.Log("Clicked");
+        SceneManager.LoadScene("Instructions");
+    }
+
+    private void OnQuit()
+    {
+        Application.Quit();
+    }
+}
