9cf489e67d9f7b77ddc5b08cb59f04c9618bd718
Jon Deaves <hello@jondeaves.me>
2019-01-26 10:09:00 +0000

    feat: add loot bag that can be attached to players

---
+++ Assets/scripts/Loot/LootBag.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootBag : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> Loot;
+
+    [SerializeField]
+    private readonly int PlayerNumber = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Action " + PlayerNumber))
+        {
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, 1.5f);
+
+            foreach (Collider col in hitColliders)
+            {
+                if (col.tag == "loot" && Loot.FindAll((GameObject obj) => obj == col.gameObject).Count == 0)
+                {
+                    Debug.Log("Thief got some loot");
+                    Loot.Add(col.gameObject);
+                    col.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+
+    public void AddLoot(GameObject obj)
+    {
+        if (obj.tag == "loot")
+        {
+            Loot.Add(obj);
+        }
+    }
+}
--- Assets/scripts/movement.cs
+++ Assets/scripts/movement.cs
@@ -6,39 +6,39 @@ public class movement : MonoBehaviour
 {
     private const float INPUT_DEADZONE = 0.19f;
 
-	public float speed = 200.0f;
-	public Rigidbody rb;
+	public float speed = 150.0f;
+	private Rigidbody m_RigidBody;
 	private bool m_IsMoving;
-    public int playerNumber = 1;
+    public int PlayerNumber = 1;
 
 	// Start is called before the first frame update
 	void Start()
 	{
-		rb = GetComponent<Rigidbody>();
+		m_RigidBody = GetComponent<Rigidbody>();
         m_IsMoving = false;
     }
 
 	// Update is called once per frame
 	void Update()
 	{
-        m_IsMoving = Input.GetAxis("Horizontal " + playerNumber) > INPUT_DEADZONE || Input.GetAxis("Horizontal " + playerNumber) < -INPUT_DEADZONE ||
-            Input.GetAxis("Vertical " + playerNumber) > INPUT_DEADZONE || Input.GetAxis("Vertical " + playerNumber) < -INPUT_DEADZONE;
+        m_IsMoving = Input.GetAxis("Horizontal " + PlayerNumber) > INPUT_DEADZONE || Input.GetAxis("Horizontal " + PlayerNumber) < -INPUT_DEADZONE ||
+            Input.GetAxis("Vertical " + PlayerNumber) > INPUT_DEADZONE || Input.GetAxis("Vertical " + PlayerNumber) < -INPUT_DEADZONE;
 
         if (!m_IsMoving)
         {
-            rb.velocity = Vector3.zero;
+            m_RigidBody.velocity = Vector3.zero;
             transform.rotation = new Quaternion();
             return;
         }
 
-        // check for movment input
-        float x = (Input.GetAxis("Horizontal " + playerNumber) * speed) * Time.deltaTime;
-		float y = (Input.GetAxis("Vertical " + playerNumber) * speed) * Time.deltaTime;
-
         //update movement and rotation if we are not within our deadzone
-        rb.velocity = new Vector3(x, 0, y);
+        m_RigidBody.velocity = new Vector3(
+            (Input.GetAxis("Horizontal " + PlayerNumber) * speed) * Time.deltaTime, 
+            0,
+            (Input.GetAxis("Vertical " + PlayerNumber) * speed) * Time.deltaTime
+        );
 
-        transform.rotation = Quaternion.LookRotation(rb.velocity);
+        transform.rotation = Quaternion.LookRotation(m_RigidBody.velocity);
 
     }
 
